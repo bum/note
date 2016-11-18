98a33f1dabe13696840d0b5b1820b2c3a8b152ca
luanth <luanth@gmail.com>
2016-11-18 11:45:44 +0700

    28

--- note/ipfact/ipfact.js
+++ note/ipfact/ipfact.js
@@ -27,8 +27,7 @@ function createSummaryAndThumb(pID, url) {
 	var img = div.getElementsByTagName("img");
 	var summ = summary_noimg;
 	if (img.length >= 1) {
-		imgtag = ' <span style = "float:left;margin-right: 10px;width:250px;height:200px;background: url(' + img[0].src
-				+ ') center 0 no-repeat transparent;background-size: 250px auto;" ></span> ';
+		imgtag = ' <span class="thumb" style = "background: url(' + img[0].src + ');" ></span> ';
 		summ = summary_img
 	}
 	var summary = ' <a href = "' + url + '" >' + imgtag + ' <div>' + removeHtmlTag(div.innerHTML, summ) + ' </div>' + '</a>';
